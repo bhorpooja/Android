0b3bd24a5eccf07a05c5b1ff7d4e4d0e5ee0daa9
pooja <bhorpooja222gmail.com>
2017-10-12 15:57:47 +0530

    Login App-InterActivity Communication complete

--- sampleapp/src/main/java/com/codekul/sampleapp/MainActivity.java
+++ sampleapp/src/main/java/com/codekul/sampleapp/MainActivity.java
@@ -8,13 +8,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQ_REGISTER=1234;
     public static final int REQ_LOGIN=1235;
     public static final String KEY_LOGIN="success";
-    private TextView txtUsr;
-    private EditText edtPwd;
+    private TextView txtUsr,txtPwd;
+    String user,pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +31,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void onLogin(View view){
 
-        if(txtUsr!=null && edtPwd!=null){
-        Bundle bundle=new Bundle();
-        bundle.putString(KEY_LOGIN,"Login Successfully..Welcome..!");
-        Intent intent=new Intent(this,LoginActivity.class);
-        intent.putExtras(bundle);
-        startActivityForResult(intent,REQ_LOGIN);
-        }
-        else {
+        if (txtUsr!=null && txtPwd!=null) {
+
             Bundle bundle=new Bundle();
-            bundle.putString(KEY_LOGIN,"Login Not Successfully..First do registration..?");
+            bundle.putString(KEY_LOGIN,"Login Successfully....?");
             Intent intent=new Intent(this,LoginActivity.class);
             intent.putExtras(bundle);
             startActivityForResult(intent,REQ_LOGIN);
 
+
+        } else {
+
+            Bundle bundle=new Bundle();
+            bundle.putString(KEY_LOGIN,"Login Not Successfully..First do registration..!");
+            Intent intent=new Intent(this,LoginActivity.class);
+            intent.putExtras(bundle);
+            startActivityForResult(intent,REQ_LOGIN);
         }
 
     }
@@ -55,10 +59,12 @@ public class MainActivity extends AppCompatActivity {
             if(resultCode==RESULT_OK){
                 Bundle bundle=data.getExtras();
                 if(bundle!=null){
-                    txtUsr=findViewById(R.id.txtUsr);
-                    txtUsr.setText(bundle.getString(RegistrationActivity.RES_USER));
-                    edtPwd=findViewById(R.id.edtPwd);
-                    edtPwd.setText(bundle.getString(RegistrationActivity.RES_PWD));
+                   txtUsr=findViewById(R.id.txtUsr);
+                   user=bundle.getString(RegistrationActivity.RES_USER);
+                    txtUsr.setText(user);
+                    txtPwd=findViewById(R.id.txtPwd);
+                    pwd=bundle.getString(RegistrationActivity.RES_PWD);
+                    txtPwd.setText(pwd);
                 }
             }
 
