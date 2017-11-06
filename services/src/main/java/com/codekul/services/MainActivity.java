afa23a34d71bf60d4e93e1ea0bf7311c30758ad4
pooja <bhorpooja222gmail.com>
2017-11-06 11:06:15 +0530

    Services : Media Player

---
+++ services/src/main/java/com/codekul/services/MainActivity.java
@@ -0,0 +1,28 @@
+package com.codekul.services;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+      intent=new Intent(this,SongService.class);
+
+    }
+
+    public void onBtnStart(View view){
+        startService(intent);
+    }
+    public void onBtnStop(View view){
+        stopService(intent);
+
+    }
+}
---
+++ services/src/main/java/com/codekul/services/SongService.java
@@ -0,0 +1,34 @@
+package com.codekul.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class SongService extends Service {
+
+    private MediaPlayer mp;
+    public SongService() {
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mp=MediaPlayer.create(this,R.raw.my);
+        mp.start();
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.stop();
+        mp.release();
+
+        super.onDestroy();
+    }
+}
