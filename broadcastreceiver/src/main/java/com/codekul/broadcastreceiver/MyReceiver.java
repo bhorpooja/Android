7f69d5646af3118d81cd602d7ffdd9acf48996ad
pooja <bhorpooja222gmail.com>
2017-11-06 12:42:31 +0530

    Broadcast Receiver: Handle event through xml

---
+++ broadcastreceiver/src/main/java/com/codekul/broadcastreceiver/MyReceiver.java
@@ -0,0 +1,14 @@
+package com.codekul.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"App is not running..Airplane Mode changed..",Toast.LENGTH_SHORT).show();
+    }
+}
