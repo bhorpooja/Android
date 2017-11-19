3095527c7799bf20afcf916520e99d9fecc3395e
pooja <bhorpooja222gmail.com>
2017-11-19 12:50:41 +0530

    UI Thread - Async Task

--- uithread/src/main/java/com/codekul/uithread/MainActivity.java
+++ uithread/src/main/java/com/codekul/uithread/MainActivity.java
@@ -1,5 +1,6 @@
 package com.codekul.uithread;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
@@ -21,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnOkay(View view){
 //        threadCounter();
-        handlerCounter();
+//        handlerCounter();
+        new MyTask().execute(0,10/*params*/);
     }
 
     private void counter(){
@@ -71,4 +73,48 @@ public class MainActivity extends AppCompatActivity {
         }).start();
 
     }
+
+    private class MyTask extends AsyncTask<Integer/*params*/,String/*progress*/,Boolean/*result*/>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //UI Thread
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... params) {
+
+            //Worker Thread
+
+            for (int i=params[0];i<params[1];i++) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                publishProgress(String.valueOf(i));
+            }
+             return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+
+            //UI Thread
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            //UI Thread
+            ((TextView) findViewById(R.id.txtCount)).setText(values[0]);
+
+        }
+
+    }
+
 }
