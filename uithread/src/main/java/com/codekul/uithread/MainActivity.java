5f428f6d7e3790ad7aa60ff3eb904eb616695a3a
pooja <bhorpooja222gmail.com>
2017-11-19 11:17:51 +0530

    UI Thread - Handler

---
+++ uithread/src/main/java/com/codekul/uithread/MainActivity.java
@@ -0,0 +1,74 @@
+package com.codekul.uithread;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler=new Handler(Looper.getMainLooper());
+    }
+
+    public void btnOkay(View view){
+//        threadCounter();
+        handlerCounter();
+    }
+
+    private void counter(){
+        for(int i=0;i<100;i++){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ((TextView) findViewById(R.id.txtCount)).setText(String.valueOf(i));
+        }
+    }
+
+    private void threadCounter(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                counter();
+            }
+        }).start();
+    }
+
+    int i=0;
+
+    private void handlerCounter(){
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(i=0;i<100;i++){
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView) findViewById(R.id.txtCount)).setText(String.valueOf(i));
+
+                        }
+                    });
+
+                }
+            }
+        }).start();
+
+    }
+}
